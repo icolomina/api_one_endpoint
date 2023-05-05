dbf1d8e6e82a99675762d750a37242e0e47f6f88
Nacho <ignacio.colomina@freemediainternet.com>
2023-05-05 08:09:40 +0200

    Delete ApiOeController and creates a trait instead

--- Controller/OperationControllerTrait.php
+++ Controller/OperationControllerTrait.php
@@ -4,14 +4,13 @@ namespace Ict\ApiOneEndpoint\Controller;
 
 use Ict\ApiOneEndpoint\Model\Api\ApiInput;
 use Ict\ApiOneEndpoint\Operation\OperationHandler;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\SerializerInterface;
 
-abstract class ApiOeController extends AbstractController
+trait OperationControllerTrait
 {
-    protected function executeOperation(Request $request, SerializerInterface $serializer, OperationHandler $operationHandler): JsonResponse
+    public function executeOperation(Request $request, SerializerInterface $serializer, OperationHandler $operationHandler): JsonResponse
     {
         $apiInput  = $serializer->deserialize($request->getContent(), ApiInput::class, 'json');
         $apiOutput = $operationHandler->performOperation($apiInput);
