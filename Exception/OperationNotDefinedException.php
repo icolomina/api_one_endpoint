c4c9a8b15aef27a6b5792636aa997159be53ff87
Nacho Colomina <ignacio.colomina@gmail.com>
2023-06-07 14:34:44 +0200

    Operation not found extends not NotFoundHttpException

--- Exception/OperationNotDefinedException.php
+++ Exception/OperationNotDefinedException.php
@@ -2,11 +2,13 @@
 
 namespace Ict\ApiOneEndpoint\Exception;
 
-class OperationNotDefinedException extends \RuntimeException
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class OperationNotDefinedException extends NotFoundHttpException
 {
     public function __construct(string $operation, int $code = 0, ?\Throwable $previous = null)
     {
         $message = sprintf('Operation %s is not defined', $operation);
-        parent::__construct($message, $code, $previous);
+        parent::__construct($message, $previous, $code);
     }
 }
